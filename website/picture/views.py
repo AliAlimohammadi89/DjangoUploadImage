15881b4332d8371cf760c034135f7ebbacea112a
AliAlimohammadi89 <alimohammadia92@gmail.com>
2022-05-03 23:23:30 +0430

    add template

--- website/picture/views.py
+++ website/picture/views.py
@@ -1,11 +1,10 @@
 import os
-import random
 
-from PIL import Image
+from django.conf import settings
 from django.shortcuts import render
 
 from .forms import ImageForm
-from django.conf import settings
+
 
 def image_upload_view(request):
     """Process images uploaded by users"""
@@ -16,48 +15,31 @@ def image_upload_view(request):
             countofpic = request.POST.get('countofpic', None)
             form.save()
             img_obj = form.instance
-            imagelistName = []
-            for x in range(int(countofpic)):
-                imagelistName.append(uploadMultiImage(fileitem))
-
             imageedit(fileitem)
-            return render(request, 'index.html', {'form': form, 'img_obj': img_obj, 'imagelistName': imagelistName})
+            # return render(request, 'index.html', {'form': form, 'img_obj': img_obj, 'imagelistName': imagelistName})
+            return  render(request, 'index.html', {'form': form, 'img_obj': img_obj})
     else:
         form = ImageForm()
         return render(request, 'index.html', {'form': form})
 
 
-def uploadMultiImage(fileitem):
-    filename = fileitem.name
-    randoms = str(random.randint(1000, 99999)) + str('-')
-    newFileName = randoms + filename
-    UPLOAD_DIR = os.path.join(settings.MEDIA_ROOT, 'images', filename)
-    img = Image.open(UPLOAD_DIR)
-    img2 = img.size
-    resized_img = img.resize((500, 500))
-    UPLOAD_DIR1 = os.path.join(settings.MEDIA_ROOT, 'images', newFileName)
-    resized_img.save(UPLOAD_DIR1)
-    return (newFileName)
-
-import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from skimage import filters
-# from skimage.data import camera
-from skimage.util import compare_images
 
-def imageedit (filename):
-    print ('edit')
+
+# from skimage.data import camera
+
+def imageedit(filename):
+    print('edit')
     # path = r"1.jpg"
-    path =  os.path.join(settings.MEDIA_ROOT, 'images', filename.name)
+    path = os.path.join(settings.MEDIA_ROOT, 'images', filename.name)
     image = cv2.imread(path)
     b, g, r = cv2.split(image)  # image
     image = g
     # image = camera()
     edge_roberts = filters.roberts(image)
     edge_sobel = filters.sobel(image)
-
-
 
     fig, axes = plt.subplots(ncols=4, sharex=True, sharey=True,
                              figsize=(8, 4))
@@ -79,4 +61,3 @@ def imageedit (filename):
 
     plt.tight_layout()
     plt.show()
-
