2ed27604b8e53b7f2614a0da926ef3f127676ebd
AliAlimohammadi89 <alimohammadia92@gmail.com>
2022-05-04 23:08:40 +0430

    add multi function

--- website/picture/views.py
+++ website/picture/views.py
@@ -4,6 +4,15 @@ from django.conf import settings
 from django.shortcuts import render
 
 from .forms import ImageForm
+import matplotlib.pyplot as plt
+import cv2
+from skimage import filters
+
+
+
+# import string
+# from re import match
+# from unittest import case
 
 
 def image_upload_view(request):
@@ -12,26 +21,23 @@ def image_upload_view(request):
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             fileitem = request.FILES['image']
-            countofpic = request.POST.get('countofpic', None)
+            countofpic = int(request.POST.get('countofpic', None))
             form.save()
             img_obj = form.instance
-            imageedit(fileitem)
+            functinRun(countofpic, fileitem)
+            # imageedit(fileitem)
             # return render(request, 'index.html', {'form': form, 'img_obj': img_obj, 'imagelistName': imagelistName})
-            return  render(request, 'index.html', {'form': form, 'img_obj': img_obj})
+            return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
     else:
         form = ImageForm()
         return render(request, 'index.html', {'form': form})
 
 
-import matplotlib.pyplot as plt
-import cv2
-from skimage import filters
-
 
 # from skimage.data import camera
 
 def imageedit(filename):
-    print('edit')
+    print('edit 1')
     # path = r"1.jpg"
     path = os.path.join(settings.MEDIA_ROOT, 'images', filename.name)
     image = cv2.imread(path)
@@ -61,3 +67,43 @@ def imageedit(filename):
 
     plt.tight_layout()
     plt.show()
+
+
+def imageedit2(filename):
+    print('imageedit2 2')
+    # path = r"1.jpg"
+    path = os.path.join(settings.MEDIA_ROOT, 'images', filename.name)
+    image = cv2.imread(path)
+    b, g, r = cv2.split(image)  # image
+    image = g
+    # image = camera()
+    edge_roberts = filters.roberts(image)
+    edge_sobel = filters.sobel(image)
+
+    fig, axes = plt.subplots(ncols=3, sharex=True, sharey=True,
+                             figsize=(8, 4))
+
+    axes[0].imshow(image, cmap=plt.cm.gray)
+    axes[0].set_title('original image')
+
+    axes[1].imshow(edge_roberts, cmap=plt.cm.gray)
+    axes[1].set_title('Roberts Edge Detection')
+
+    axes[2].imshow(edge_sobel, cmap=plt.cm.gray)
+    axes[2].set_title('Sobel Edge Detection')
+
+    for ax in axes:
+        ax.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def functinRun(i, filename):
+    if i == 1:
+        imageedit(filename)
+        return
+
+    elif i == 2:
+        imageedit2(filename)
+        return
